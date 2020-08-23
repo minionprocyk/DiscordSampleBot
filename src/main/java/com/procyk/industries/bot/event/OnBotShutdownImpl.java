3a0275c923ba4f71de5a0177b8f68cfa08722b77
Minion Procyk <minion.procyk@gmail.com>
2020-08-23 14:24:38 -0400

    guarantee application shutdown

--- src/main/java/com/procyk/industries/bot/event/OnBotShutdownImpl.java
+++ src/main/java/com/procyk/industries/bot/event/OnBotShutdownImpl.java
@@ -9,7 +9,10 @@ import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.EventListener;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class OnBotShutdownImpl extends ListenerAdapter implements EventListener {
     private final ExecutorService executorService;
@@ -20,8 +23,17 @@ public class OnBotShutdownImpl extends ListenerAdapter implements EventListener 
     @Override
     public void onShutdown(ShutdownEvent event) {
         super.onShutdown(event);
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                executorService.shutdownNow();
+                System.out.println("System did not exit properly");
+                System.exit(-1);
+            }
+        };
+        timer.schedule(timerTask, TimeUnit.SECONDS.toMillis(10));
         executorService.shutdown();
-
         //remove all wave files created by CreateWaveFileTask
         try {
             Files.walk(Paths.get("/tmp"), 0, FileVisitOption.FOLLOW_LINKS)
